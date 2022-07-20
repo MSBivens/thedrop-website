eec5b5ecaca5a0475dc4c1a03bcfe7a34ef1bfe4
MSBivens <mikebiv95@gmail.com>
2022-07-19 20:53:05 -0500

    dotenv + axios setup

--- thedrop-website/next.config.js
+++ thedrop-website/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+require("dotenv").config();
 
-module.exports = nextConfig
+module.exports = {
+  swcMinify: true,
+  reactStrictMode: true,
+  env: {
+    ALCHEMY_API_KEY: process.env.ALCHEMY_API_KEY,
+    ALCHEMY_API_KEY_URL: process.env.ALCHEMY_API_KEY_URL,
+  },
+};
