981616eec952a14594c9b0e9c584e569ad9e792a
MSBivens <mikebiv95@gmail.com>
2022-07-20 00:54:00 -0500

    alchemy sdk install & redo of querry script

--- thedrop-website/next.config.js
+++ thedrop-website/next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-require("dotenv").config();
+// require("dotenv").config();
+// import "dotenv/config";
 
 module.exports = {
   swcMinify: true,
   reactStrictMode: true,
-  env: {
-    ALCHEMY_API_KEY: process.env.ALCHEMY_API_KEY,
-    ALCHEMY_API_KEY_URL: process.env.ALCHEMY_API_KEY_URL,
-  },
 };
---
+++ thedrop-website/scripts/axios-script.js
@@ -0,0 +1,20 @@
+// Executable with: node axios-script.js
+// getNFTs
+import axios from "axios";
+
+// Replace with your Alchemy API key:
+const apiKey = "demo";
+const baseURL = `https://eth-mainnet.alchemyapi.io/nft/v2/${apiKey}/getNFTs/`;
+// Replace with the wallet address you want to query for NFTs:
+const ownerAddr = "0xF5FFF32CF83A1A614e15F25Ce55B0c0A6b5F8F2c";
+
+// Construct the axios request:
+var config = {
+  method: "get",
+  url: `${baseURL}?owner=${ownerAddr}`,
+};
+
+// Make the request and print the formatted response:
+axios(config)
+  .then((response) => console.log(JSON.stringify(response.data, null, 2)))
+  .catch((error) => console.log(error));
