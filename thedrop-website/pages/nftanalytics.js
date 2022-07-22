3b47b666305e8f40fccafdcdcb3d232fdfd5922a
MSBivens <mikebiv95@gmail.com>
2022-07-21 21:42:28 -0500

    fetch method testing

--- thedrop-website/pages/nftanalytics.js
+++ thedrop-website/pages/nftanalytics.js
@@ -1,8 +1,27 @@
 import { useState } from "react";
 import NFTCard from "../components/nftCard";
+// import fetch from "fetch";
 
 const nftanalytics = () => {
   const [NFTs, setNFTs] = useState([]);
+
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  const apiKey = "demo";
+  const baseURL = `https://eth-mainnet.alchemyapi.io/nft/v2/${apiKey}/getNFTsForCollection`;
+  const contractAddr = "0x61fce80d72363b731425c3a2a46a1a5fed9814b2";
+  const tokenId = "0x1ea2";
+  const withMetadata = "false";
+  const fetchURL = `${baseURL}?contractAddress=${contractAddr}&startToken=${tokenId}&withMetadata=${withMetadata}`;
+
+  fetch(fetchURL, requestOptions)
+    .then((response) => response.json())
+    .then((response) => JSON.stringify(response, null, 2))
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
 
   return (
     <div className="flex flex-col items-center justify-center pt-40 py-8 gap-y-3">
@@ -12,7 +31,7 @@ const nftanalytics = () => {
             "disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"
           }
           onClick={() => {
-            nftsForCollection;
+            fetch;
           }}
         >
           Let's go!{" "}
