1de5dcc1664f61fa7a47419de45f7bbcc1188a70
MSBivens <mikebiv95@gmail.com>
2022-07-23 12:18:18 -0500

    file cleanup

--- thedrop-website/pages/nftanalytics.jsx
+++ thedrop-website/pages/nftanalytics.jsx
@@ -4,47 +4,20 @@ import { NFTCard } from "../components/nftCard";
 // Testing contract address: 0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d
 
 export default function NftAnalytics() {
-  const [collection, setCollectionAddress] = useState("");
-  const [NFTs, setNFTs] = useState([]);
-
-  // Change content displays
+  // Toggle content displays
   const [displayGallery, setDisplayGallery] = useState(false);
   const handleGallery = () => {
     setDisplayGallery(!displayGallery);
   };
 
-  // const [wallet, setWalletAddress] = useState("");
-  // const [fetchForCollection, setFetchForCollection] = useState(false);
-
-  // const fetchNFTs = async () => {
-  //   let nfts;
-  //   console.log("fetching nfts");
-  //   const api_key = "kygVl8vLhN5FdS4zpb2MHi5pcGNLQytg";
-  //   const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
-
-  //   if (!collection.length) {
-  //     var requestOptions = {
-  //       method: "GET",
-  //     };
-
-  //     const fetchURL = `${baseURL}?owner=${wallet}`;
-
-  //     nfts = await fetch(fetchURL, requestOptions).then((data) => data.json());
-  //   } else {
-  //     console.log("fetching nfts for collection owned by address");
-  //     const fetchURL = `${baseURL}?owner=${wallet}&contractAddresses%5B%5D=${collection}`;
-  //     nfts = await fetch(fetchURL, requestOptions).then((data) => data.json());
-  //   }
-
-  //   if (nfts) {
-  //     console.log("nfts:", nfts);
-  //     setNFTs(nfts.ownedNfts);
-  //   }
-  // };
   // General API const
   const api_key = "kygVl8vLhN5FdS4zpb2MHi5pcGNLQytg";
   const api_call = "getNFTsForCollection/";
   const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/${api_call}`;
+
+  //Data const
+  const [collection, setCollectionAddress] = useState("");
+  const [NFTs, setNFTs] = useState([]);
 
   // API Calls
   const fetchNFTsForCollection = async () => {
@@ -66,16 +39,6 @@ export default function NftAnalytics() {
   return (
     <div className="flex flex-col items-center justify-center pt-32 py-8 gap-y-3">
       <div className="flex flex-col w-full justify-center items-center gap-y-2">
-        {/* <input
-          disabled={fetchForCollection}
-          className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50"
-          onChange={(e) => {
-            setWalletAddress(e.target.value);
-          }}
-          value={wallet}
-          type={"text"}
-          placeholder="Add your wallet address"
-        ></input> */}
         <input
           className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50"
           onChange={(e) => {
@@ -85,16 +48,6 @@ export default function NftAnalytics() {
           type={"text"}
           placeholder="Add the collection address"
         ></input>
-        {/* <label className="text-gray-600 ">
-          <input
-            onChange={(e) => {
-              setFetchForCollection(e.target.checked);
-            }}
-            type={"checkbox"}
-            className="mr-2"
-          ></input>
-          Fetch for collection
-        </label> */}
         <button
           className={
             "disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"
